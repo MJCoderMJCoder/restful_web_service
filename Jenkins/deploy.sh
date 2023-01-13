8439c5f086c8dbe6bdd20eaea675ed8a284b9d5f
liuzhifeng01 <liuzhifeng01@inspur.com>
2023-01-13 22:24:04 +0800

    优化部署stage

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo docker build -t restful_web_service:0.0.1 .
-sudo docker run -d -p 80:80 --name restful_web_service --network dockercomposedemo_default restful_web_service:0.0.1
+su root
+docker build -t restful_web_service:0.0.1 .
+docker run -d -p 80:80 --name restful_web_service --network dockercomposedemo_default restful_web_service:0.0.1
