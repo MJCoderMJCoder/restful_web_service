e77caca84e77783e9b7dbe4d904666856f18f11d
liuzhifeng01 <liuzhifeng01@inspur.com>
2023-01-14 17:24:43 +0800

    优化部署脚本：创建docker用户组，并把user用户加进该用户组（直接按官方文档顺序执行即可，如果已经新建用户组则跳过此步）

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+current_user='getent passwd \`who\` | head -n 1 | cut -d : -f 1'
+echo "Current user：${current_user}"
 #给docker.sock添加读写权限
 sudo chmod +777 /var/run/docker.sock
-echo "Current user："
-getent passwd `who` | head -n 1 | cut -d : -f 1
 #创建docker用户组.
 egrep "^docker" /etc/group >& /dev/null
 if [ $? -ne 0 ]
@@ -10,7 +10,7 @@ then
     sudo groupadd docker
 fi
 #将当前用户加入到docker用户组.
-sudo usermod -aG docker jenkins
+sudo usermod -aG docker ${current_user}
 #激活更新用户组信息:
 newgrp docker
 docker build -t restful_web_service:0.0.1 .
