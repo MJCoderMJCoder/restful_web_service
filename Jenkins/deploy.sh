4f022838984b4b617ac7b5109d467809b911dedb
liuzhifeng01 <liuzhifeng01@inspur.com>
2023-01-14 23:50:44 +0800

    优化容器名称已经存在时的逻辑处理

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #给docker.sock添加读写权限
 mv ./build/libs/restful_web_service-0.0.1-SNAPSHOT.jar ./Jenkins/
-# gradle清理构建的信息
-./gradlew clean
 chmod +777 /var/run/docker.sock
-if [ `docker inspect restful_web_service 2> /dev/null | grep '"Name": "/restful_web_service"'` != "" ]
+container_name=`docker inspect restful_web_service 2> /dev/null | grep '"Name": "/restful_web_service"'`
+echo "${container_name}"
+if [ ${#container_name} -gt 0 ]
 then
   echo "Stop restful_web_service"
   docker stop restful_web_service
