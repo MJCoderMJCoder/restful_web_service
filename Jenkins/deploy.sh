77899e3e8fec23f09db3d4f67cad169306515bb5
liuzhifeng01 <liuzhifeng01@inspur.com>
2023-01-14 23:11:37 +0800

    添加容器存在时的逻辑处理

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -4,5 +4,10 @@ mv ./build/libs/restful_web_service-0.0.1-SNAPSHOT.jar ./Jenkins/
 # gradle清理构建的信息
 ./gradlew clean
 chmod +777 /var/run/docker.sock
+if [[ "$(docker inspect restful_web_service 2> /dev/null | grep '"Name": "/restful_web_service"')" != "" ]]
+then
+  docker stop restful_web_service
+  docker rm -f restful_web_service
+fi
 docker build -t restful_web_service:0.0.1 ./Jenkins/
 docker run -d -p 80:80 --name restful_web_service --network dockercomposedemo_default restful_web_service:0.0.1
