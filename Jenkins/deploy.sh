b0895a693564e3b854ac6b52846ee0673cb3bb1b
liuzhifeng01 <liuzhifeng01@inspur.com>
2023-01-14 23:57:47 +0800

    部署成功后删除无用的镜像

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -10,3 +10,4 @@ then
 fi
 docker build -t restful_web_service:0.0.1 ./Jenkins/
 docker run -d -p 80:80 --name restful_web_service --network dockercomposedemo_default restful_web_service:0.0.1
+docker image prune
