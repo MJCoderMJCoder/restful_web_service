b7074bc4ae55c87d6bd276d167d4e12af6b2f53e
liuzhifeng01 <liuzhifeng01@inspur.com>
2023-01-14 22:30:48 +0800

    优化部署脚本：修改docker build 的 上下文路径

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #给docker.sock添加读写权限
-sudo chmod +777 /var/run/docker.sock
-docker build -t restful_web_service:0.0.1 .
+chmod +777 /var/run/docker.sock
+docker build -t restful_web_service:0.0.1 ./Jenkins/
 docker run -d -p 80:80 --name restful_web_service --network dockercomposedemo_default restful_web_service:0.0.1
