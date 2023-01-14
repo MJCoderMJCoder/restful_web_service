ed9a1b560c055d07a2e03a1a6585940c829bcff8
liuzhifeng01 <liuzhifeng01@inspur.com>
2023-01-14 23:54:32 +0800

    测试CI/CD

--- src/main/java/com/spring_boot/restful_web_service/controller/GreetingController.java
+++ src/main/java/com/spring_boot/restful_web_service/controller/GreetingController.java
@@ -21,7 +21,7 @@ public class GreetingController {
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public Greeting index(@RequestParam(value = "name", required = false, defaultValue = "地球") String username) {
+    public Greeting index(@RequestParam(value = "name", required = false, defaultValue = "灿灿") String username) {
         return new Greeting(COUNTER.incrementAndGet(),
                 String.format("你好，%s！", username));
     }
