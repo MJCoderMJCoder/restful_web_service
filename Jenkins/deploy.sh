8300125a0c6969ae1dc684cbc6ab20b0876d5c00
liuzhifeng01 <liuzhifeng01@inspur.com>
2023-01-14 21:08:05 +0800

    优化部署脚本：给docker.sock添加读写权限

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 #给docker.sock添加读写权限
 sudo chmod +777 /var/run/docker.sock
-current_user=`getent passwd \`who\` | head -n 1 | cut -d : -f 1`
-echo "Current user：${current_user}"
-#创建docker用户组.
-current_group=`egrep "^docker" /etc/group`
-if [ ${#current_group} -gt 0 ]
-then
-   echo "The docker user group already exists"
-else
-    sudo groupadd docker
-fi
-#将当前用户加入到docker用户组.
-sudo usermod -aG docker ${current_user}
-#激活更新用户组信息:
-newgrp docker
 docker build -t restful_web_service:0.0.1 .
 docker run -d -p 80:80 --name restful_web_service --network dockercomposedemo_default restful_web_service:0.0.1
