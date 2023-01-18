fc18fefea6613200bc2dd5f82e81365032ad00c7
liuzhifeng01 <liuzhifeng01@inspur.com>
2023-01-18 12:23:21 +0800

    在docker-compose.yml文件所在的正确目录下执行docker compose命令

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -7,7 +7,7 @@ then
   docker stop restful_web_service
   docker rm -f restful_web_service
 fi
-pwd
+cd ./Jenkins/
 docker-compose up -d
 #docker build -t restful_web_service:0.0.1 ./Jenkins/
 #docker run -d -p 80:80 --name restful_web_service --network dockercomposedemo_default --restart=always restful_web_service:0.0.1
