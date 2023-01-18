b274f90067f36d0c77e6529f4ee67bb6da16bc12
liuzhifeng01 <liuzhifeng01@inspur.com>
2023-01-18 11:54:35 +0800

    将原来的docker启动命令，改为使用docker-compose

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #给docker.sock添加读写权限
 mv ./build/libs/restful_web_service-0.0.1-SNAPSHOT.jar ./Jenkins/
-chmod +777 /var/run/docker.sock
 container_name=`docker inspect restful_web_service 2> /dev/null | grep '"Name": "/restful_web_service"'`
 if [ ${#container_name} -gt 0 ]
 then
   docker stop restful_web_service
   docker rm -f restful_web_service
 fi
+pwd
 docker-compose up -d
 #docker build -t restful_web_service:0.0.1 ./Jenkins/
 #docker run -d -p 80:80 --name restful_web_service --network dockercomposedemo_default --restart=always restful_web_service:0.0.1
