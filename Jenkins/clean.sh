9572856595aebccd0809dacf65c6117328752b1e
liuzhifeng01 <liuzhifeng01@inspur.com>
2023-01-18 14:29:29 +0800

    在部署时清理容器和镜像，以确保docker-compose命令每次执行后都重新生成新镜像和新容器。

--- Jenkins/clean.sh
+++ Jenkins/clean.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# gradle清理构建的信息
+# gradle清理无用的镜构建信息
 ./gradlew clean
+# 清理无用的镜像
+docker image prune -f
--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-#给docker.sock添加读写权限
 mv ./build/libs/restful_web_service-0.0.1-SNAPSHOT.jar ./Jenkins/
+# 清理容器
 container_name=`docker inspect restful_web_service 2> /dev/null | grep '"Name": "/restful_web_service"'`
 if [ ${#container_name} -gt 0 ]
 then
   docker stop restful_web_service
   docker rm -f restful_web_service
-  # 清理无用的镜像
-  docker image prune -f
+fi
+# 清理镜像
+image_id=`docker images "restful_web_service" -q 2> /dev/null`
+if [ ${#image_id} -gt 0 ]
+then
+  docker rmi -f ${image_id}
 fi
 cd ./Jenkins/
 docker-compose up -d
